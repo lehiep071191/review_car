0136d7ca85a8b0b3a2d94daec5389968800cc72e
lehiep071191 <good.boy.0991@gmail.com>
2020-09-03 20:02:34 +0700

    thêm, sửa , xóa oto

---
+++ app/controllers/admin/cars_controller.rb
@@ -0,0 +1,55 @@
+class Admin::CarsController < ApplicationController
+	before_action :require_admin
+  before_action :find_car, only: [:show, :edit, :update, :destroy]
+  def index
+  	@cars = Car.all
+  end	
+  def new
+  	@car = Car.new
+  end
+  def create
+  	@car = Car.new(car_params)
+    if @car.save
+      flash[:success] = "D"
+      redirect_to admin_cars_path
+    else
+      render 'new'
+    end  
+  end	
+  def show
+
+  end	
+  def edit
+  	@car = Car.find_by id:params[:id]
+
+  end
+  def update
+  	if @car.update(car_params)
+      flash[:success] = "updated"
+      redirect_to admin_cars_path(@car)
+    else
+      render 'edit'
+    end	
+  end
+  def destroy
+    	@car.destroy
+    flash[:success] = "post deleted"
+    redirect_to request.referrer || root_url
+  end	
+  private 
+  def car_params	
+  	params.require(:car).permit(:name, :color ,:type_car,:brand,:status)
+  end	
+
+  def require_admin
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
+
+  def find_car
+  	@car = Car.find_by id:params[:id] 
+        redirect_to root_path unless @car
+  end	
+
+end
